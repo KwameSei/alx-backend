b9b60feda460de6bac142c4b8584d2cfea945076
KwameSei <nathanielosei92@gmail.com>
2023-04-18 04:25:10 +0000

    Returning data from imported csv file

---
+++ 0x00-pagination/1-simple_pagination.py
@@ -0,0 +1,44 @@
+#!/usr/bin/env python3
+""" Returning indexes of pages """
+import csv
+import math
+from typing import List
+
+
+index_range = __import__('0-simple_helper_function').index_range
+
+
+class Server:
+    """Server class to paginate a database of popular baby names.
+    """
+    DATA_FILE = "Popular_Baby_Names.csv"
+
+    def __init__(self):
+        self.__dataset = None
+
+    def dataset(self) -> List[List]:
+        """Cached dataset
+        """
+        if self.__dataset is None:
+            with open(self.DATA_FILE) as f:
+                reader = csv.reader(f)
+                dataset = [row for row in reader]
+            self.__dataset = dataset[1:]
+
+        return self.__dataset
+
+    def get_page(self, page: int = 1, page_size: int = 10) -> List[List]:
+        """ Returning a page with data """
+        assert isinstance(page, int) and page > 0, \
+            "page must be an integer greater than 0"
+        assert isinstance(page_size, int) and page_size > 0, \
+            "page_size must be an integer greater than 0"
+
+        data = self.dataset()   # getting data using dataset method
+        if len(data) == 0:
+            return []
+
+        index_start, index_end = index_range(page, page_size)
+        if index_start >= len(data):
+            return []
+        return data[index_start:index_end]
