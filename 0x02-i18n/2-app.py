144688a26c76946a9d088af9721e14121eaee37e
KwameSei <nathanielosei92@gmail.com>
2023-04-25 19:09:46 +0000

    Getting locale language from request

--- 0x02-i18n/2-app.py
+++ 0x02-i18n/2-app.py
@@ -2,6 +2,7 @@
 """ Creating Flask application """
 from flask import Flask, render_template
 from flask_babel import Babel
+from flask import request
 
 app = Flask(__name__)
 babel = Babel(app)  # Initialize Babel
@@ -15,6 +16,15 @@ class Config(object):   # Configure available languages
 
 
 app.config.from_object(Config)  # Load config class
+
+
+#  Invoking the Babel object’s localeselector decorator to translate languages
+@babel.localeselector
+def get_locale_language():
+    """ Getting the locale language for each request"""
+    app_config = app.config['LANGUAGES']    # Get languages from config file
+    locale_request = request.accept_languages   # Get languages from request
+    return locale_request.best_match(app_config)   # Return best language match
 
 
 @app.route('/', strict_slashes=False)
