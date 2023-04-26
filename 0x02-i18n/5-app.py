d3839b5bb50dc6fed4b7a79742aba101344b703f
KwameSei <nathanielosei92@gmail.com>
2023-04-26 05:56:37 +0000

    Mock user login system

--- 0x02-i18n/5-app.py
+++ 0x02-i18n/5-app.py
@@ -1,7 +1,16 @@
 #!/usr/bin/env python3
 """ Creating Flask application """
-from flask import Flask, render_template, request
+from flask import Flask, render_template, request, g
 from flask_babel import Babel, gettext, get_locale
+
+
+users = {
+    1: {"name": "Balou", "locale": "fr", "timezone": "Europe/Paris"},
+    2: {"name": "Beyonce", "locale": "en", "timezone": "US/Central"},
+    3: {"name": "Spock", "locale": "kg", "timezone": "Vulcan"},
+    4: {"name": "Teletubby", "locale": None, "timezone": "Europe/London"},
+}
+
 
 app = Flask(__name__)
 babel = Babel(app)  # Initialize Babel
@@ -31,10 +40,31 @@ def get_locale():
     return locale_request  # Return best language match
 
 
+def get_user(user_id):
+    """ Function to return user dictionary or None """
+    user = users.get(int(user_id))
+    # if user_id in users:
+    #     return users.get(user_id)
+    # return None
+    if user:
+        return user
+    return None
+
+@app.before_request
+def before_request():
+    """ Find a user if any, and set it as a global on flask.g.user """
+    user_id = request.args.get('login_as')
+    if user_id:
+        user = get_user(int(user_id))
+        if user:
+            g.user = user
+    else:
+        g.user = None
+
 @app.route('/', strict_slashes=False)
 def index():
     """ Creating index page """
-    return render_template('4-index.html')
+    return render_template('5-index.html')
 
 
 if __name__ == '__main__':
